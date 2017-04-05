f61f5c9992887862e4f416dd95a98ec932475a7b
Guillaume Lamothe <201453922@edu.clg.qc.ca>
2017-04-05 11:05:52 -0400

    ajout de la classe commentaires

---
+++ LidgrenXnaGameClient/LidgrenXnaGameClient/Commentaires.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XnaGameClient
+{
+    class Commentaires
+    {
+        ///allo ici c'est la classe pour des commentaire 
+        ///
+        /// premiere idée : mettre un beam qui suit le joueur pour montrer sa position plus facilement;
+    }
+}
