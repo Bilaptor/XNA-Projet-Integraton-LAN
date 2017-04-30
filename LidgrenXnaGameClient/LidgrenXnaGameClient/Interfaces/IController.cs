62910caee0c4d7fcbe9c4132d4291baa3a1770c5
Bilaptor <Bilaptor@BILAPTOR-PC>
2017-04-30 16:12:49 -0400

    modification control avec souris

---
+++ LidgrenXnaGameClient/LidgrenXnaGameClient/Interfaces/IController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace XnaGameClient
+{
+   interface IController
+   {
+      Vector3 GetPosition();
+      Vector3 GetDirection();
+      Vector3 GetDirectionVu();
+   }
+}
