32baec23e60b156a720819c9bfcdba3c959d6e6f
Juneau <Juneau@DESKTOP-9EFI254>
2017-05-13 20:55:37 -0400

    dfsdf

--- LidgrenXnaGameClient/LidgrenXnaGameClient/Menu2.cs
+++ LidgrenXnaGameClient/LidgrenXnaGameClient/Menu2.cs
@@ -17,38 +17,33 @@ namespace XnaGameClient
     /// </summary>
     public class Menu2 : Microsoft.Xna.Framework.DrawableGameComponent
     {
+
+        public bool Pause { get; private set; }
+        bool accepte = true;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         InputManager GestionInput { get; set; }
         Game Jeu { get; set; }
-        public bool Pause { get; private set; }
 
 
-        bool accepte = true;
+        cButton btnExit;
+        cButton btnAnnuler;
+        cButton btnCommandes;
+        cButton btnPlay;
+
 
         enum GameStat
         {
-            Host,
-            Join,
-            //Options,
             Mainmenu,
             Commandes,
         }
         GameStat CurrentGameStat = GameStat.Mainmenu;
 
-        // Screen size ajustement
+        //Ajustement de l'écran
         int screenWidth = 800;
         int screenHeight = 480;
-        //int screenWidth = 1600;
-        //int screenHeight = 900;
-
-
-        cButton btnHost;
-        cButton btnJoin;
-        //cButton btnOptions;
-        cButton btnExit;
-        cButton btnAnnuler;
-        cButton btnCommandes;
+        
 
 
         public Menu2(Game game, GraphicsDeviceManager pere, bool pause)
@@ -59,72 +54,50 @@ namespace XnaGameClient
             graphics.IsFullScreen = false;
             Jeu.Content.RootDirectory = "Content";
             Pause = pause;
-
             SetPause(pause);
-
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+
         public override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            //Services.AddService(typeof(InputManager), GestionInput);
             Pause = true;
             SetPause(true);
             base.Initialize();
         }
 
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+       
         protected override void LoadContent()
         {
-            //SetPause(true);
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Screen ajustment
+            //Ajustement de l'écran
             graphics.PreferredBackBufferWidth = screenWidth;
             graphics.PreferredBackBufferHeight = screenHeight;
             graphics.ApplyChanges();
             Jeu.IsMouseVisible = true;
 
+            
+            //Création du bouton Play
+            btnPlay = new cButton(Jeu.Content.Load<Texture2D>("button model Play"), graphics.GraphicsDevice);
+            btnPlay.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 125));
 
-            btnHost = new cButton(Jeu.Content.Load<Texture2D>("button model host"), graphics.GraphicsDevice);
-            btnHost.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 100));
 
-            //btnOptions = new cButton(Content.Load<Texture2D>("button model OPTIONS"), graphics.GraphicsDevice);
-            //btnOptions.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 300));
-
+            //Création du bouton Exit
             btnExit = new cButton(Jeu.Content.Load<Texture2D>("button model EXIT"), graphics.GraphicsDevice);
-            btnExit.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 400));
+            btnExit.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 325));
 
-            btnJoin = new cButton(Jeu.Content.Load<Texture2D>("button model JOIN"), graphics.GraphicsDevice);
-            btnJoin.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 200));
 
+            //Création du bouton Annuler
             btnAnnuler = new cButton(Jeu.Content.Load<Texture2D>("button model ANNULER"), graphics.GraphicsDevice);
             btnAnnuler.setPosition(new Vector2(10, 400));
 
-            btnCommandes = new cButton(Jeu.Content.Load<Texture2D>("button model COMMANDES"), graphics.GraphicsDevice);
-            btnCommandes.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 300));
-            ///350,300 center
-        }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
+            //Création du bouton Commandes
+            btnCommandes = new cButton(Jeu.Content.Load<Texture2D>("button model COMMANDES"), graphics.GraphicsDevice);
+            btnCommandes.setPosition(new Vector2((screenWidth / 2) - (screenWidth / 10), 225));
         }
+       
 
         private void SetPause(bool enPause)
         {
@@ -140,14 +113,13 @@ namespace XnaGameClient
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-
             MouseState mouse = Mouse.GetState();
 
             switch (CurrentGameStat)
             {
                 case GameStat.Mainmenu:
-                    btnHost.Update(mouse);
-                    if (btnHost.isClicked)
+                    btnPlay.Update(mouse);
+                    if (btnPlay.isClicked)
                     {
                         Jeu.IsMouseVisible = false;
                         accepte = false;
@@ -155,11 +127,6 @@ namespace XnaGameClient
                         SetPause(false);
                     }
 
-                    //btnOptions.Update(mouse);
-                    //if (btnOptions.isClicked == true)
-                    //{
-                    //    CurrentGameStat = GameStat.Options;
-                    //}
 
                     btnExit.Update(mouse);
                     if (btnExit.isClicked == true)
@@ -167,14 +134,6 @@ namespace XnaGameClient
                         Jeu.Exit();
                     }
 
-                    btnJoin.Update(mouse);
-                    if (btnJoin.isClicked == true)
-                    {
-                        Jeu.IsMouseVisible = false;
-                        accepte = false;
-                        Pause = false;
-                        SetPause(false);
-                    }
 
                     btnCommandes.Update(mouse);
                     if (btnCommandes.isClicked == true)
@@ -184,28 +143,7 @@ namespace XnaGameClient
 
                     break;
 
-                //case GameStat.Options:
-
-
-                //    btnAnnuler.Update(mouse);
-                //    if (btnAnnuler.isClicked == true)
-                //    {
-                //        CurrentGameStat = GameStat.Mainmenu;
-                //    }
-
-                //    break;
-
-                case GameStat.Host:
-
-
-                    break;
-
-                case GameStat.Join:
-
-
-
-                    break;
-
+       
                 case GameStat.Commandes:
 
 
@@ -217,11 +155,6 @@ namespace XnaGameClient
 
                     break;
             }
-
-            //if (GestionInput.EstNouveauClicGauche())
-            //{
-            //    CurrentGameStat = GameStat.Mainmenu;
-            //}
 
             base.Update(gameTime);
         }
@@ -236,38 +169,18 @@ namespace XnaGameClient
             {
                 GraphicsDevice.Clear(Color.CornflowerBlue);
 
-
                 spriteBatch.Begin();
 
                 switch (CurrentGameStat)
                 {
                     case GameStat.Mainmenu:
                         spriteBatch.Draw(Jeu.Content.Load<Texture2D>("background 1"), new Rectangle(0, 0, screenWidth, screenHeight), Color.Red);
-                        btnHost.Draw(spriteBatch);
-                        //btnOptions.Draw(spriteBatch);
+                        btnPlay.Draw(spriteBatch);
                         btnExit.Draw(spriteBatch);
-                        btnJoin.Draw(spriteBatch);
                         btnCommandes.Draw(spriteBatch);
 
                         break;
 
-                    //case GameStat.Options:
-                    //    spriteBatch.Draw(Content.Load<Texture2D>("background 1"), new Rectangle(0, 0, screenWidth, screenHeight), Color.Red);
-                    //    btnAnnuler.Draw(spriteBatch);
-
-                    //    break;
-
-                    case GameStat.Host:
-                        spriteBatch.Draw(Jeu.Content.Load<Texture2D>("background 1"), new Rectangle(0, 0, screenWidth, screenHeight), Color.Red);
-                        btnAnnuler.Draw(spriteBatch);
-
-                        break;
-
-                    case GameStat.Join:
-                        spriteBatch.Draw(Jeu.Content.Load<Texture2D>("background 1"), new Rectangle(0, 0, screenWidth, screenHeight), Color.Red);
-                        btnAnnuler.Draw(spriteBatch);
-
-                        break;
 
                     case GameStat.Commandes:
                         spriteBatch.Draw(Jeu.Content.Load<Texture2D>("back ground menu"), new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
